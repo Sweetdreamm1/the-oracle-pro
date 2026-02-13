62ce5b4aefa975feaab8a3bf33d97104f8580348
Sweetdreamm1 <mointer0159@gmail.com>
2026-02-14 02:45:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,5 @@ def send_message(recipient_id, message_text):
     requests.post(url, json=payload)
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
